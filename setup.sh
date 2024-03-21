ab4e716d8e7b433aa1fcfbe9de74907df7eb586f
Antonio Ballesteros <antonio.ballesteros@outlook.com>
2024-03-20 22:11:20 -0700

    full website update

--- setup.sh
+++ setup.sh
@@ -5,10 +5,4 @@ rm -rfv /cse/web/homes/antonio6/{*,.*}
 git clone https://github.com/ballesterosa/Personal-Website.git
 mv -f /cse/web/homes/antonio6/Personal-Website/{.,}* /cse/web/homes/antonio6/
 rmdir /cse/web/homes/antonio6/Personal-Website
-rm -rf index.html
-rm -rf style.ssh
-rm -rf /cse/web/homes/antonio6/img/{*,.*}
-rm -rfv /cse/web/homes/antonio6/img/{*,.*}
-rmdir /cse/web/homes/antonio6/img
-mv -f /cse/web/homes/antonio6/html5up-read-only/{.,}* /cse/web/homes/antonio6/
-rmdir /cse/web/homes/antonio6/html5up-read-only
+rm -rf /cse/web/homes/antonio6/html5up-read-only
