01d6c91198d9bb5dd0d54389bfe5ef5c27baf040
Antonio Ballesteros <antonio.ballesteros@outlook.com>
2024-03-20 22:18:17 -0700

    fix setup bash script

--- setup.sh
+++ setup.sh
@@ -1,8 +1,7 @@
 #! /bin/sh
 cd /cse/web/homes/antonio6/
-rm -rf /cse/web/homes/antonio6/{*,.*}
-rm -rfv /cse/web/homes/antonio6/{*,.*}
+rm -rfv /cse/web/homes/antonio6/.
 git clone https://github.com/ballesterosa/Personal-Website.git
-mv -f /cse/web/homes/antonio6/Personal-Website/{.,}* /cse/web/homes/antonio6/
-rmdir /cse/web/homes/antonio6/Personal-Website
+cp -rf /cse/web/homes/antonio6/Personal-Website/. /cse/web/homes/antonio6/
+rm -rf /cse/web/homes/antonio6/Personal-Website/
 rm -rf /cse/web/homes/antonio6/html5up-read-only
