7263b688771e479385ec697449df43455a34ebec
Antonio Ballesteros <antonio.ballesteros@outlook.com>
2024-03-20 22:22:43 -0700

    update repo name

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 cd /cse/web/homes/antonio6/
 rm -rfv /cse/web/homes/antonio6/.
-git clone https://github.com/ballesterosa/Personal-Website.git
-cp -rf /cse/web/homes/antonio6/Personal-Website/. /cse/web/homes/antonio6/
-rm -rf /cse/web/homes/antonio6/Personal-Website/
+git clone https://github.com/ballesterosa/website.git
+cp -rf /cse/web/homes/antonio6/website/. /cse/web/homes/antonio6/
+rm -rf /cse/web/homes/antonio6/website/
 rm -rf /cse/web/homes/antonio6/html5up-read-only
